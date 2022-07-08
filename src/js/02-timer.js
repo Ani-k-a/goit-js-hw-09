0c4316c9a0ad2c6c328f091169e3417759875df2
Hanna Lytvynenko <97547547+Ani-k-a@users.noreply.github.com>
2022-07-08 10:41:06 +0200

    Update timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,11 +2,13 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
-const inputEl = document.querySelector('input[type="text"]');
-const btnEl = document.querySelector('button[data-start]');
-let timeLeftToDate = 0;
+const inputDataEl = document.querySelector('input[type="text"]');
+const buttonStartEl = document.querySelector('button[data-start]');
+const dataBox = document.querySelector('.timer');
+const emoji = String.fromCodePoint(0x1f621);
 
-btnEl.setAttribute('disabled', 'disabled');
+let diff = null;
+buttonStartEl.setAttribute('disabled', 'disabled');
 
 const options = {
   enableTime: true,
@@ -14,68 +16,65 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-
-    if (selectedDates[0] < new Date()) {
-      Notiflix.Notify.failure('Please choose a date in the future');
+    if (selectedDates[0] <= new Date()) {
+      buttonStartEl.setAttribute('disabled', 'disabled');
+      Notiflix.Report.failure(
+        `Ohhhh...${emoji}`,
+        'Please choose a date in the future',
+        'Alona, try again!!!'
+      );
     } else {
-      btnEl.removeAttribute('disabled', 'disabled');
+      buttonStartEl.removeAttribute('disabled');
     }
-
-    timeLeftToDate = selectedDates[0] - new Date();
-    timeCounter(timeLeftToDate);
+    diff = selectedDates[0] - new Date();
   },
 };
 
-flatpickr(inputEl, options);
+flatpickr(inputDataEl, options);
 
-function timeCounter(time) {
-  timeLeftToDate = time - 1000;
-
-  convertMs(timeLeftToDate);
-}
-
-btnEl.addEventListener('click', event => {
-  btnEl.setAttribute('disabled', 'disabled');
-
-  let timerId = setInterval(() => {
-    timeCounter(timeLeftToDate);
-    const Obj = convertMs(timeLeftToDate);
-    const dataObj = addLeadingZero(Obj);
-    for (let key in dataObj) {
-      const spanEl = (document.querySelector(
-        `span[data-${key}]`
-      ).textContent = `${dataObj[key]}`);
+buttonStartEl.addEventListener('click', () => {
+  let setIntervalId = setInterval(() => {
+    const { days, hours, minutes, seconds } = convertMs(diff);
+    diff -= 1000;
+    document.querySelector('[data-days]').textContent = addLeadingZero(days);
+    document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
+    document.querySelector('[data-minutes]').textContent =
+      addLeadingZero(minutes);
+    document.querySelector('[data-seconds]').textContent =
+      addLeadingZero(seconds);
+    // dataBox.style.color = getRandomHexColor();
+    // dataBox.style.backgroundColor = getRandomHexColor();
+    function addLeadingZero(value) {
+      if (value > 99) {
+        return String(value);
+      }
+      return String(value).padStart(2, '0');
+    }
+    if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
+      clearInterval(setIntervalId);
+      return;
     }
   }, 1000);
-
-  if (timeLeftToDate === 0) {
-    clearInterval(timerId);
-    return;
-  }
 });
 
 function convertMs(ms) {
+  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
+  // Remaining days
   const days = Math.floor(ms / day);
-
+  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-
+  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-
+  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
-
-function addLeadingZero(obj) {
-  for (let key in obj) {
-    obj[key] = String(obj[key]).padStart(2, 0);
-  }
-
-  return obj;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
